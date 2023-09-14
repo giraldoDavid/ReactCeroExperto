3380400a22e0c5200741e05b3435266be3a5463a
David Giraldo <david.girald0@hotmail.com>
2023-09-14 17:54:21 -0500

    feat(className): GifItem Created

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,14 +1,34 @@
-
-import {getGifs} from '../helpers/getGifs.js'
+import { useEffect, useState } from 'react';
+import { GifItem } from './GifItem.jsx';
+import { getGifs } from '../helpers/getGifs.js';
 
 export const GifGrid = ({ category }) => {
     
+    const [images, setImages] = useState([])
+
+    const getIamges = async() => {
+        const newImages = await getGifs( category )
+        setImages(newImages);
+    }
     
-    getGifs(category);
+    useEffect( ()=>{
+        getIamges()
+    }, [  ] )
+
+    
     return (
         <>
             <h3>{ category }</h3>
-
+            <div className='card-grid'>
+                { images.map( (image) => {
+                    return (
+                        <GifItem 
+                            key={ image.id } 
+                            { ...image }
+                        />
+                    )
+                }) }
+            </div>
         </>
     )
 };
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 export const getGifs = async(category) => {
         
     const API_KEY = "se04DYJeIyiVxGfKZN5Ge5EsVQ5Jazcf";
-    const URL = `https://api.giphy.com/v1/gifs/search?api_key=${ API_KEY }&q=${ category }&limit=20`;
+    const URL = `https://api.giphy.com/v1/gifs/search?api_key=${ API_KEY }&q=${ category }&limit=10`;
 
     const response = await fetch(URL);
     const { data } = await response.json();
@@ -14,7 +14,6 @@ export const getGifs = async(category) => {
         url: img.images.downsized_medium.url
     }))
 
-    console.log("gifs", gifs);
     return gifs;
-}
+};
 
